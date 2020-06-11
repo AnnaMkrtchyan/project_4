082033cbc25e73486c5c697b9ef05ba124c72bcd
AnnaMkrtchyan <58968451+AnnaMkrtchyan@users.noreply.github.com>
2020-06-11 12:22:56 +0400

    Add files via upload

---
+++ project_4/js/script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector(".menu-icon");
+let menuToggle = document.querySelector(".header-menu")
+
+menuIcon.addEventListener("click", function(){
+    menuToggle.classList.toggle("active");
+})
+
